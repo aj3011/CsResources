b825529dfc4232d659f12a711fa93247269904c3
aj3011 <ashreyjairath@gmail.com>
2021-03-28 11:57:40 -0700

    Created the second page

--- src/HomePage/Dividers.js
+++ src/HomePage/Dividers.js
@@ -30,12 +30,12 @@ function Dividers()
 
     return (
         <div className='divider'>
-
         <NavBar className='navbar'/>
+        <div className='ques'>What would you like to get help with today?</div>
         <div className='resources rheading'>
         <React.Fragment>
-         <Link to="/Resources" style={{ textDecoration: 'none' }}>
-          Learning Resources
+         <Link to="/Resources" style={{ textDecoration: 'none',color:'white' }}>
+          Languages
           <p className='para'>Explore a variety of learning resources based on the choice of your interest</p>
          </Link>
         </React.Fragment>
@@ -44,14 +44,14 @@ function Dividers()
         <div className='projects pheading'>
         <React.Fragment>
         {/* <Link> will essentially return a standard <a> tag, which is why we apply textDecoration rule there. */}
-        <Link to ='/ProjectIdeas' style={{ textDecoration: 'none' }}>
-           Project Ideas
+        <Link to ='/ProjectIdeas' style={{ textDecoration: 'none',color:'white' }}>
+           Technologies
            <p className='para'>Discover a list of interesting topics on different domains</p>
           
            </Link>
         </React.Fragment>
         </div>
-          
+         
         </div>
     )
 }
